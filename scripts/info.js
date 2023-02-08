1ae0fb9428e4d901da2bd6ca7c3b8b2b7ed540ca
Zura Iakobishvili <iakobishvili.zura@gmail.com>
2023-02-09 00:01:02 +0400

    Add photo upload function

--- scripts/info.js
+++ scripts/info.js
@@ -31,23 +31,47 @@ insertData(phoneNumber,phoneNumberValue,'phone');
 insertData(textarea,aboutMe,'aboutme');
 
 
-// Image upload function
-var uploadedImage  = '';
-photo.addEventListener('change', function(){
-    const reader = new FileReader();
-    reader.addEventListener('load', ()=>{
-        uploadedImage = reader.result;
-        photoValue.style.backgroundImage = `url(${uploadedImage})`
-        photoValue.style.borderRadius = '50%'
-        localStorage.setItem(photo,photo.value)
-    });
-    reader.readAsDataURL(this.files[0]);
-    photo.value = localStorage.getItem('photo')
+// Image upload 
+
+const uploadImage = document.getElementById('photoUpload');
+
+photoUpload.addEventListener('change' , () => {
+    const file = photoUpload.files[0]; 
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.addEventListener('load', () => {
+        const url = fileReader.result       
+        const img = new Image();
+        img.src = url;
+        photoValue.appendChild(img);
+    })
+
 })
+
+photoUpload.addEventListener('change' , () => {
+  const file = photoUpload.files[0]; 
+  const fileReader = new FileReader();
+  fileReader.readAsDataURL(file);
+
+  fileReader.addEventListener('load', () => {
+      const url = fileReader.result
+      localStorage.setItem('image', url);
+  })
+});
+
+const url = localStorage.getItem('image');
+const img = new Image();
+img.src = url;
+photoValue.appendChild(img);
+
+
 
 // Validation
 
 const pattern = /^[ა-ჰ]+$/;
+const successIcon = document.getElementById('firstNameSuccessIcon');
+const dangerIcon = document.getElementById('firstNameDangerIcon');
 
 if (localStorage.getItem("firstNameStyle")) {
     firstName.setAttribute("style", localStorage.getItem("firstNameStyle"));
@@ -57,16 +81,23 @@ firstName.addEventListener("input", function () {
   if (firstName.value.match(pattern) && firstName.value.length > 2) {
     firstName.setAttribute("style", "border: 1px solid #98E37E;");
     localStorage.setItem("firstNameStyle","border: 1px solid #98E37E;");
+    
+    successIcon.style.visibility = 'visible';
+    dangerIcon.style.visibility = 'hidden'
+   
   } else {
     firstName.setAttribute("style", "border: 1px solid #EF5050;");
     localStorage.setItem("firstNameStyle","border: 1px solid #EF5050;");
+    
+    dangerIcon.style.visibility = 'visible'
+    successIcon.style.visibility = 'hidden';
   }
 
     if(firstName.value.length == 0) {
         firstName.setAttribute("style", "border: 1px solid #BCBCBC;");
         localStorage.setItem("firstNameStyle","border: 1px solid #BCBCBC;");
     }
-    
+
 });
 
 if (localStorage.getItem("lastNameStyles")) {
@@ -107,5 +138,26 @@ email.addEventListener("input", function () {
     if(email.value.length == 0) {
         email.setAttribute("style", "border: 1px solid #BCBCBC;");
         localStorage.setItem("emailStyles","border: 1px solid #BCBCBC;");
+      }
+  });
+
+  const geoNumberPattern = /^\+995\d{9}$/
+
+  if (localStorage.getItem("phoneStyles")) {
+    phoneNumber.setAttribute("style", localStorage.getItem("phoneStyles"));
+  }
+
+  phoneNumber.addEventListener("input", function () {
+    if (phoneNumber.value.match(geoNumberPattern)) {
+      phoneNumber.setAttribute("style", "border: 1px solid #98E37E;");
+      localStorage.setItem("phoneStyles","border: 1px solid #98E37E;");
+    } else {
+        phoneNumber.setAttribute("style", "border: 1px solid #EF5050;");
+        localStorage.setItem("phoneStyles","border: 1px solid #EF5050;");
+    }
+  
+    if(phoneNumber.value.length == 0) {
+        phoneNumber.setAttribute("style", "border: 1px solid #BCBCBC;");
+        localStorage.setItem("phoneStyles","border: 1px solid #BCBCBC;");
       }
   });
